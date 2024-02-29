98e5c10e84a0a8bc595ca565d21b1ebe8cef6573
nsalday <nsalday@up.edu.ph>
2024-03-01 03:57:04 +0800

    docs: Added new comments and also added new test cases

--- exer3/alday_exer3.js
+++ exer3/alday_exer3.js
@@ -1,9 +1,19 @@
+/*
+    Name: Neil Vincet S. Alday
+    Section: UV-3L
+    Exercise 3: JavaScript Basics part 1
+*/
+
+// This function validates the password if it's valid or not
 function validatePassword(p1, p2){
+    // Checks if p1 is equal to p2. If not, return false
     if (p1 == p2){
+        // Loops through p1's characters
         for(var i = 0; i < p1.length; i++){
+            // If there is a number present in the string, check if there is at least one lowercase letter, at least one uppecase letter, and the string is at least 8 in length...
             if(!isNaN(parseInt(p1[i]))){
                 if ((p1 != p1.toLowerCase()) && (p1 != p1.toUpperCase()) && (p1.length >= 8)){
-                    return true
+                    return true // If conditions are met, return true
                 }
             }
         }
@@ -12,27 +22,35 @@ function validatePassword(p1, p2){
     return false
 }
 
+// Reverses a given string
 function reversePassword(str){
-    var newStr = ""
+    var newStr = "" // Variable to store the new string
 
+    // Traverses the string in a reverse order
     for(var i = str.length - 1; i >= 0; i--){
-        newStr += str[i]
+        newStr += str[i] // Increment the current character to newStr
     }
 
-    return newStr
+    return newStr // Return newStr
 }
 
+// Stores a new password of the user
 function storePassword(name, p1, p2){
-    var newPass = 'name: "' + name + '"' + ', newpassword: "'
-    if(p1 == p2){
-        newPass += reversePassword(p1) + '"'
+    var newPass = 'name: "' + name + '"' + ', newpassword: "' // newPass is added with the name and other strings
+    if(p1 == p2){ // If the two passwords match...
+        newPass += reversePassword(p1) + '"' //add the new password to newPass with its reversed state
     } else {
-        newPass += p1 + '"'
+        newPass += p1 + '"' // If they don't match, the newPass is still the p1
     }
 
-    return newPass
+    return newPass // return newPass
 }
 
+// Test Cases
 console.log(validatePassword("Hey12345", "Hey12345"))
 console.log(reversePassword("Hey12345"))
 console.log(storePassword("Neil", "Hey12345", "ey12345"))
+console.log(storePassword("John", "Pass1234", "Pass1234"))
+console.log(storePassword("John", "Pass123", "Pass12345"))
+console.log(validatePassword("HELLO1234", "HELLO1234"))
+console.log(validatePassword("Hello1234", "Hello1234"))
