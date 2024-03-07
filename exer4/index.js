c06cb76d00599752a5c97be0aebc44f01e8e70a9
nsalday <nsalday@up.edu.ph>
2024-03-08 04:14:30 +0800

    chore: updated comments and updated parameters for addAccount()

--- exer4/index.js
+++ exer4/index.js
@@ -1,28 +1,35 @@
+//Imports
 const fs = require('fs');
-const {v4 : uuidv4} = require('uuid')
+const {v4 : uuidv4} = require('uuid');
 const validator = require('validator');
 
 //Generates a unique ID for a given user
 function generateUniqueID(firstName, lastName){
     var newFirst = firstName.slice(0,1); //Gets the first character of the user
-    const userID = uuidv4().slice(0,8) //Gets the first 8 characters in the generated ID
+    const userID = uuidv4().slice(0,8); //Gets the first 8 characters in the generated ID
     var newName = newFirst.toLowerCase() + lastName.toLowerCase() + userID; //Concatenates newFirst and userID
 
-    return newName //returns newName
+    return newName; //Returns newName
 }
 
-function addAccount(firstName, lastName, email, age){
+//Creates an account from the given user details and saves it in an account
+function addAccount([firstName, lastName, email, age]){
+    //Check if the parameters exist
     if (Boolean(firstName) && Boolean(lastName) && Boolean(email) && Boolean(age)){
+        //Check if the strings are not empty strings
         if(firstName !== '' && lastName !== '' && email !== ''){    
+            //Checks if the email is valid and the age is at least 18
             if (validator.isEmail(email) && age >= 18){
-                var user = firstName + "," + lastName + "," + email + "," + age + "," + generateUniqueID(firstName, lastName)
+                //Generates a new user
+                var user = firstName + "," + lastName + "," + email + "," + age + "," + generateUniqueID(firstName, lastName);
+                //Saves details in users.txt
                 fs.appendFileSync("users.txt", user + '\n', "UTF-8",{'flags': 'a+'});
-                return true
+                return true; //return true
             }
         }
     }
 
-    return false
+    return false; //If the conditions are not met, return false
 }
 
-module.exports = {addAccount, generateUniqueID}
+module.exports = {addAccount, generateUniqueID}; //export
--- exer4/test.js
+++ exer4/test.js
@@ -9,16 +9,16 @@
 const index = require('./index.js');
 
 //Test Cases
-console.log(index.addAccount("Neil", "Alday", "nsalday@gmail.com", 18)) //True
-console.log(index.addAccount("Neil", "", "nsalday@gmail.com", 18)) //False
-console.log(index.addAccount("", "Alday", "nsalday@gmail.com", 19)) //False
-console.log(index.addAccount("Neil", "Alday", "", 19)) //False
-console.log(index.addAccount("Neil", "Alday", "nsalday@gmail.com")) //False
-console.log(index.addAccount("Neil", "Alday", 18)) //False
-console.log(index.addAccount("Alday", "nsalday@gmail.com", 18)) //False
-console.log(index.addAccount("Neil", "Alday", 18)) //False
-console.log(index.addAccount("Neil", 18)) //False
-console.log(index.addAccount("Neil")) //False
-console.log(index.addAccount()) //False
-console.log(index.addAccount("Neil", "Alday", "nsalday@gmail.com", 17)) //False
-console.log(index.addAccount("Mark", "Day", "lain@gmail.com", 20)) //True
+console.log(index.addAccount(["Neil", "Alday", "nsalday@gmail.com", 18])) //True
+console.log(index.addAccount(["Neil", "", "nsalday@gmail.com", 18])) //False
+console.log(index.addAccount(["", "Alday", "nsalday@gmail.com", 19])) //False
+console.log(index.addAccount(["Neil", "Alday", "", 19])) //False
+console.log(index.addAccount(["Neil", "Alday", "nsalday@gmail.com"])) //False
+console.log(index.addAccount(["Neil", "Alday", 18])) //False
+console.log(index.addAccount(["Alday", "nsalday@gmail.com", 18])) //False
+console.log(index.addAccount(["Neil", "Alday", 18])) //False
+console.log(index.addAccount(["Neil", 18])) //False
+console.log(index.addAccount(["Neil"])) //False
+console.log(index.addAccount([])) //False
+console.log(index.addAccount(["Neil", "Alday", "nsalday@gmail.com", 17])) //False
+console.log(index.addAccount(["Mark", "Day", "lain@gmail.com", 20])) //True
