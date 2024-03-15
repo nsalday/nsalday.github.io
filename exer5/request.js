8e19f8ac000950b1b62c8ea64faaa9c2351e2610
nsalday <nsalday@up,edu.ph>
2024-03-15 20:07:01 +0800

    chore: added a new function to read books.txt and updated some of the initial files

--- exer5/request.js
+++ exer5/request.js
@@ -1,18 +1,40 @@
+import needle from "needle";
+
 needle.post(
-    {bookName: "Cookingn't?!",
-    isbn: "20",
-    author: "Neil Alday",
-    yearPublished: "03-15-2024"
-    },
-    (err, res) => {
-        console.log(res.body);
+  "http://localhost:3000/add-book",
+  {
+    bookName: "Harry Potter and the Chamber of Secrets",
+    isbn: "0-7475-3849-2",
+    author: "J.K Rowling",
+    yearPublished: "",
+  },
+  (err, res) => {
+    if (err) {
+      console.error(err);
+      return;
     }
-)
-
-needle.get('http://localhost:3000/find-by-isbn-author?isbn=978-0-7475-3269-9&author=J.K+Rowling', (err, res) => {
     console.log(res.body);
-});
+  }
+);
 
-needle.get('http://localhost:3000/find-by-author?author=J.K+Rowling', (err, res) => {
+needle.get(
+  "http://localhost:3000/find-by-isbn-author?isbn=978-0-7475-3269-9&author=J.K+Rowling",
+  (err, res) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
     console.log(res.body);
-});
+  }
+);
+
+needle.get(
+  "http://localhost:3000/find-by-author?author=J.K+Rowling",
+  (err, res) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    console.log(res.body);
+  }
+);
--- exer5/server.js
+++ exer5/server.js
@@ -1,50 +1,81 @@
-import { appendFileSync } from "fs";
-
+import { appendFileSync, readFileSync } from "fs";
 import express from "express";
 
-const app = express()
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
+function readBooksFromFile() {
+  const data = readFileSync("books.txt", "utf-8");
+  const lines = data.split("\n");
+  const books = [];
+  for (const line of lines) {
+    const [bookName, isbn, author, yearPublished] = line.split(",");
+    books.push({ bookName, isbn, author, yearPublished });
+  }
+  return books;
+}
 
 app.post("/add-book", (req, res) => {
   const { bookName, isbn, author, yearPublished } = req.body;
 
-  if (bookName == "" || isbn == "" || author != "" || yearPublished != "") {
-    res.status(400).json({message: "Not all the given fields are non-empty"});
+  if (!bookName || !isbn || !author || !yearPublished) {
+    return res.json({ success: false });
   }
 
   appendFileSync(
     "books.txt",
-    req.body.bookName +
-      "," +
-      req.body.isbn +
-      "," +
-      req.body.author +
-      "," +
-      req.body.yearPublished +
-      "\n",
+    `${bookName},${isbn},${author},${yearPublished}\n`,
     "UTF-8",
     { flags: "a+" }
   );
+
+  return res.json({ success: true });
 });
 
 app.get("/find-by-isbn-author", (req, res) => {
-    const {isbn, author} = req.query;
-    
-    if(isbn == '' || author == ''){
-        res.status(400).json({message: "Not all the given fields are non-empty"});
-    }
+  const { isbn, author } = req.query;
+
+  if (!isbn || !author) {
+    res
+      .status(400)
+      .json({ message: "ISBN and Author parameters are required." });
+    return;
+  }
+
+  const books = readBooksFromFile();
+  const matchingBooks = books.filter(
+    (book) => book.isbn === isbn && book.author === author
+  );
+
+  if (matchingBooks.length === 0) {
+    res.status(404).json({ message: "Book not found." });
+    return;
+  }
+
+  res.json(matchingBooks);
 });
 
-app.get("/find-by-isbn-author", (req, res) => {
-    const {author} = req.query;
+app.get("/find-by-author", (req, res) => {
+  const { author } = req.query;
 
-    if(author == ''){
-        res.status(400).json({message: "Not all the given fields are non-empty"});
-    }
+  if (!author) {
+    res.status(400).json({ message: "Author input is required." });
+    return;
+  }
+
+  const books = readBooksFromFile();
+  const matchingBooks = books.filter((book) => book.author === author);
+
+  if (matchingBooks.length === 0) {
+    res.status(404).json({ message: "No books found by the author." });
+    return;
+  }
+
+  res.json(matchingBooks);
 });
 
-// this tells our server to listen to the port 3000
-// we can also pass an optional callback function to execute after the server starts
+// This tells our server to listen to port 3000
 app.listen(3000, () => {
   console.log("Server started at port 3000");
 });
